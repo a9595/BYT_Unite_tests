93ba028a3cfe80b1d4a1a982ba46b6a9bb1eba00
Andrii Kovalchuk <tieorange@gmail.com>
2015-11-26 18:45:24 +0100

    currency global value

--- src/b_Money/Money.java
+++ src/b_Money/Money.java
@@ -1,107 +1,139 @@
 package b_Money;
 
 public class Money implements Comparable {
-	private int amount;
-	private Currency currency;
+    private int amount;
+    private Currency currency;
 
-	/**
-	 * New Money
-	 * @param amount	The amount of money
-	 * @param currency	The currency of the money
-	 */
-	Money (Integer amount, Currency currency) {
-		this.amount = amount;
-		this.currency = currency;
-	}
-	
-	/**
-	 * Return the amount of money.
-	 * @return Amount of money in Double type.
-	 */
-	public Integer getAmount() {
-		return amount;
-	}
-	
-	/**
-	 * Returns the currency of this Money.
-	 * @return Currency object representing the currency of this Money
-	 */
-	public Currency getCurrency() {
-		return currency;
-	}
-	
-	/**
-	 * Returns the amount of the money in the string form "(amount) (currencyname)", e.g. "10.5 SEK".
-	 * Recall that we represent decimal numbers with integers. This means that the "10.5 SEK" mentioned
-	 * above is actually represented as the integer 1050
-	 *  @return String representing the amount of Money.
-	 */
-	public String toString() {
-		return amount + " " + currency.getName();
-	}
-	
-	/**
-	 * Gets the universal value of the Money, according the rate of its Currency.
-	 * @return The value of the Money in the "universal currency".
-	 */
-	public Integer universalValue() {
-		
-	}
-	
-	/**
-	 * Check to see if the value of this money is equal to the value of another Money of some other Currency.
-	 * @param other The other Money that is being compared to this Money.
-	 * @return A Boolean indicating if the two monies are equal.
-	 */
-	public Boolean equals(Money other) {
-		
-	}
-	
-	/**
-	 * Adds a Money to this Money, regardless of the Currency of the other Money.
-	 * @param other The Money that is being added to this Money.
-	 * @return A new Money with the same Currency as this Money, representing the added value of the two.
-	 * (Remember to convert the other Money before adding the amounts)
-	 */
-	public Money add(Money other) {
-		
-	}
+    /**
+     * New Money
+     *
+     * @param amount   The amount of money
+     * @param currency The currency of the money
+     */
+    Money(Integer amount, Currency currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
-	/**
-	 * Subtracts a Money from this Money, regardless of the Currency of the other Money.
-	 * @param other The money that is being subtracted from this Money.
-	 * @return A new Money with the same Currency as this Money, representing the subtracted value.
-	 * (Again, remember converting the value of the other Money to this Currency)
-	 */
-	public Money sub(Money other) {
-		
-	}
-	
-	/**
-	 * Check to see if the amount of this Money is zero or not
-	 * @return True if the amount of this Money is equal to 0.0, False otherwise
-	 */
-	public Boolean isZero() {
-		
-	}
-	/**
-	 * Negate the amount of money, i.e. if the amount is 10.0 SEK the negation returns -10.0 SEK
-	 * @return A new instance of the money class initialized with the new negated money amount.
-	 */
-	public Money negate() {
-		
-	}
-	
-	/**
-	 * Compare two Monies.
-	 * compareTo is required because the class implements the Comparable interface.
-	 * (Remember the universalValue method, and that Integers already implement Comparable).
-	 * Also, since compareTo must take an Object, you will have to explicitly downcast it to a Money.
-	 * @return 0 if the values of the monies are equal.
-	 * A negative integer if this Money is less valuable than the other Money.
-	 * A positive integer if this Money is more valuiable than the other Money.
-	 */
-	public int compareTo(Object other) {
-		
-	}
+    /**
+     * Return the amount of money.
+     *
+     * @return Amount of money in Double type.
+     */
+    public Integer getAmount() {
+        return amount;
+    }
+
+    /**
+     * Returns the currency of this Money.
+     *
+     * @return Currency object representing the currency of this Money
+     */
+    public Currency getCurrency() {
+
+        return currency;
+    }
+
+    /**
+     * Returns the amount of the money in the string form "(amount) (currencyname)", e.g. "10.5 SEK".
+     * Recall that we represent decimal numbers with integers. This means that the "10.5 SEK" mentioned
+     * above is actually represented as the integer 1050
+     *
+     * @return String representing the amount of Money.
+     */
+    public String toString() {
+        return amount + " " + currency.getName();
+    }
+
+    /**
+     * Gets the universal value of the Money, according the rate of its Currency.
+     *
+     * @return The value of the Money in the "universal currency".
+     */
+    public Integer universalValue() {
+        return currency.universalValue(amount);
+    }
+
+    /**
+     * Check to see if the value of this money is equal to the value of another Money of some other Currency.
+     *
+     * @param other The other Money that is being compared to this Money.
+     * @return A Boolean indicating if the two monies are equal.
+     */
+    public Boolean equals(Money other) {
+        if (other.getAmount() == this.getAmount()
+                && other.getCurrency().equals(this))
+            return true;
+        else return false;
+    }
+
+    /**
+     * Adds a Money to this Money, regardless of the Currency of the other Money.
+     *
+     * @param other The Money that is being added to this Money.
+     * @return A new Money with the same Currency as this Money, representing the added value of the two.
+     * (Remember to convert the other Money before adding the amounts)
+     */
+    public Money add(Money other) {
+        //TODO
+        Integer convertedMoneyValue = other.getCurrency().valueInThisCurrency(other.getAmount(), this.getCurrency());
+        Money sum = new Money(convertedMoneyValue - this.getAmount(), this.getCurrency());
+        return sum;
+    }
+
+    /**
+     * Subtracts a Money from this Money, regardless of the Currency of the other Money.
+     *
+     * @param other The money that is being subtracted from this Money.
+     * @return A new Money with the same Currency as this Money, representing the subtracted value.
+     * (Again, remember converting the value of the other Money to this Currency)
+     */
+    public Money sub(Money other) {
+        //TODO
+        Integer convertedMoneyValue = other.getCurrency().valueInThisCurrency(other.getAmount(), this.getCurrency());
+        Money sum = new Money(convertedMoneyValue + this.getAmount(), this.getCurrency());
+        return sum;
+    }
+
+    /**
+     * Check to see if the amount of this Money is zero or not
+     *
+     * @return True if the amount of this Money is equal to 0.0, False otherwise
+     */
+    public Boolean isZero() {
+        if (getAmount().equals(0.0))
+            return true;
+        else return false;
+    }
+
+    /**
+     * Negate the amount of money, i.e. if the amount is 10.0 SEK the negation returns -10.0 SEK
+     *
+     * @return A new instance of the money class initialized with the new negated money amount.
+     */
+    public Money negate() {
+        Money negatedMoney = new Money(this.amount - (this.amount * 2), this.getCurrency());
+        return negatedMoney;
+
+    }
+
+    /**
+     * Compare two Monies.
+     * compareTo is required because the class implements the Comparable interface.
+     * (Remember the universalValue method, and that Integers already implement Comparable).
+     * Also, since compareTo must take an Object, you will have to explicitly downcast it to a Money.
+     *
+     * @return 0 if the values of the monies are equal.
+     * A negative integer if this Money is less valuable than the other Money.
+     * A positive integer if this Money is more valuiable than the other Money.
+     */
+    public int compareTo(Object other) {
+        Money paramMoney = (Money) other;
+        if (this.getAmount() == paramMoney.getAmount())
+            return 0;
+        else if (this.getAmount() < paramMoney.getAmount())
+            return -1;
+        else
+            return 1;
+    }
 }
--- test/b_Money/CurrencyTest.java
+++ test/b_Money/CurrencyTest.java
@@ -7,7 +7,11 @@ import org.junit.Test;
 
 public class CurrencyTest {
 	Currency SEK, DKK, NOK, EUR;
-	
+
+    Double rate_SEK = 0.15;
+    Double rate_DKK = 0.20;
+    Double rate_EUR = 1.5;
+
 	@Before
 	public void setUp() throws Exception {
 		/* Setup currencies with exchange rates */
@@ -18,27 +22,43 @@ public class CurrencyTest {
 
 	@Test
 	public void testGetName() {
-		fail("Write test case here");
+		//fail("Write test case here");
+        String msg = "wrong currency name";
+        assertEquals(msg, "SEK", SEK.getName());
 	}
 	
 	@Test
 	public void testGetRate() {
-		fail("Write test case here");
-	}
+		//fail("Write test case here");
+        String msg = "wrong currency rate";
+        assertEquals(msg, rate_SEK, SEK.getRate());
+
+    }
 	
 	@Test
 	public void testSetRate() {
-		fail("Write test case here");
-	}
+        String msg = "wrong currency set rate";
+        Double newRate = 0.77;
+
+        SEK.setRate(newRate);
+        assertEquals(msg, newRate, SEK.getRate());
+
+    }
 	
 	@Test
 	public void testGlobalValue() {
-		fail("Write test case here");
+		//TODO
+        //fail("Write test case here");
+        String msg = "wrong test of GlobalValue";
+        assertTrue(msg, SEK.universalValue(100).equals(15.0));
 	}
 	
 	@Test
 	public void testValueInThisCurrency() {
-		fail("Write test case here");
+
+        //fail("Write test case here");
+        String msg = "wrong ValueInThisCurrency"; //TODO
+        assertTrue(msg, EUR.valueInThisCurrency(100, SEK).equals(10));
 	}
 
 }
