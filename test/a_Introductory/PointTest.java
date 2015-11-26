83ef4aff4d29932ccc9c5a213c91df13a1494865
Andrii Kovalchuk <tieorange@gmail.com>
2015-11-26 13:13:55 +0100

    currency and money almost done

--- test/a_Introductory/PointTest.java
+++ test/a_Introductory/PointTest.java
@@ -2,36 +2,38 @@ package a_Introductory;
 
 
 import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class PointTest {
-	Point p1, p2, p3;
-	
-	@Before
-	public void setUp() throws Exception {
-		p1 = new Point(7, 9);
-		p2 = new Point(-3, -30);
-		p3 = new Point(-10, 3);
-	}
-	
-	public void testAdd() {
-		Point res1 = p1.add(p2);
-		Point res2 = p1.add(p3);
-		
-		assertEquals(4.0, res1.x);
-		assertEquals(-21.0, res1.y);
-		assertEquals(-3.0, res2.x);
-		assertEquals(12.0, res2.x);
-	}
-	
-	public void testSub() {
-		Point res1 = p1.sub(p2);
-		Point res2 = p1.sub(p3);
-		
-		assertEquals(4, res1.x);
-		assertEquals(-21, res1.y);
-		assertEquals(-3, res2.x);
-		assertEquals(12, res2.x);
-	}
+    Point p1, p2, p3;
+
+    @Before
+    public void setUp() throws Exception {
+        p1 = new Point(7, 9);
+        p2 = new Point(-3, -30);
+        p3 = new Point(-10, 3);
+    }
+
+    public void testAdd() {
+        Point res1 = p1.add(p2);
+        Point res2 = p1.add(p3);
+
+        assertEquals((Integer) 4, res1.x);
+        assertEquals((Integer) 21, res1.y);
+        assertEquals((Integer) 3, res2.x);
+        assertEquals((Integer) 12, res2.x);
+    }
+
+    public void testSub() {
+        Point res1 = p1.sub(p2);
+        Point res2 = p1.sub(p3);
+        Integer i1 = -3;
+
+        assertEquals((Integer) 4, res1.x);
+        assertEquals(new Integer(-21), res1.y);
+        assertEquals(new Integer(-3), res2.x);
+        assertEquals(new Integer(12), res2.x);
+    }
 
 }
